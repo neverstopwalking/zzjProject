096bb8adf133b938fde300221a7cac9648673aa1
neverstopwalking <1315590024@qq.com>
2024-06-16 21:46:50 +0800

    初始化

---
+++ src/main/java/com/example/zzjproject/ZzjProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.zzjproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzjProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzjProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zzjproject/ZzjProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zzjproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzjProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
